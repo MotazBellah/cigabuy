a167b5bed5049f778ee252cee85291443d16a085
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-29 13:16:50 +0200

    Handle the pagination

--- cigabuy/spiders/consumer_electronics.py
+++ cigabuy/spiders/consumer_electronics.py
@@ -7,12 +7,13 @@ class ConsumerElectronicsSpider(scrapy.Spider):
     start_urls = ['https://www.cigabuy.com/consumer-electronics-c-56_75-pg-1.html']
 
     def parse(self, response):
+        # Get the div the contain all info about the product
         div_wrapper = response.xpath("//div[@class='p_box_wrapper']")
-        for i in div_wrapper:
-            name = i.xpath(".//a[@class='p_box_title']/text()").get()
-            link = i.xpath(".//a[@class='p_box_title']/@href").get()
-            price = i.xpath(".//div[@class='p_box_price']/span[1]/text()").get()
-            original_price = i.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+        for product in div_wrapper:
+            name = product.xpath(".//a[@class='p_box_title']/text()").get()
+            link = product.xpath(".//a[@class='p_box_title']/@href").get()
+            price = product.xpath(".//div[@class='p_box_price']/span[1]/text()").get()
+            original_price = product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
 
             yield {
                 "title": name,
@@ -20,3 +21,8 @@ class ConsumerElectronicsSpider(scrapy.Spider):
                 "dicsounted_price": price,
                 "original_price": original_price,
             }
+        # Get the nextPage link
+        next_page = response.xpath("(//a[@class='nextPage'])[1]/@href").get()
+        # If found, call parse function on the url
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
