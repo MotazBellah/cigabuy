0c5272fc512f163758fcb571f57cbc12267335fc
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-29 13:02:27 +0200

    Get the title, link, discounted price and original price for each item

--- cigabuy/spiders/consumer_electronics.py
+++ cigabuy/spiders/consumer_electronics.py
@@ -3,8 +3,20 @@ import scrapy
 
 class ConsumerElectronicsSpider(scrapy.Spider):
     name = 'consumer_electronics'
-    allowed_domains = ['www.cigabuy.com/consumer-electronics-c-56_75-pg-1.html']
-    start_urls = ['http://www.cigabuy.com/consumer-electronics-c-56_75-pg-1.html/']
+    allowed_domains = ['www.cigabuy.com']
+    start_urls = ['https://www.cigabuy.com/consumer-electronics-c-56_75-pg-1.html']
 
     def parse(self, response):
-        pass
+        div_wrapper = response.xpath("//div[@class='p_box_wrapper']")
+        for i in div_wrapper:
+            name = i.xpath(".//a[@class='p_box_title']/text()").get()
+            link = i.xpath(".//a[@class='p_box_title']/@href").get()
+            price = i.xpath(".//div[@class='p_box_price']/span[1]/text()").get()
+            original_price = i.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+
+            yield {
+                "title": name,
+                'url': link,
+                "dicsounted_price": price,
+                "original_price": original_price,
+            }
